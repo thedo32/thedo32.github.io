9b22c5fb3a780c798becc43b24a621ed8808faaa
Jeffri <jeffriargon@hotmail.com>
2024-03-03 14:38:27 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Pollute.py

--- Pollute.py
+++ Pollute.py
@@ -100,7 +100,7 @@ left_cl, main_cl= st.columns([1,8])
 with left_cl:
      containup = st.container()
      containup.float()
-     containup.markdown("[UP UP UP](#pendahuluan)", unsafe_allow_html=True)
+     containup.markdown("[🆙 UP 🆙](#pendahuluan)", unsafe_allow_html=True)
 
      with st.container(border=True):
         st.markdown("<h5 style='text-align: left; color: orange;'>Section:</h5>", unsafe_allow_html=True)
